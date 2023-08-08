9a5a15bd60353a0c15e025d03df8f8b62a783881
chikombe <chikombe.ecy@gmail.com>
2023-08-08 14:08:50 -0700

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+def magic_calculation(a, b, c):
+    """A function that matches the provided bytecode."""
+    if a < b):
+        return (c)
+    if c > b):
+        return (a + b)
+    return (a * b - c)
