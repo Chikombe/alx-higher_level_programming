f2160498573e8e1ca3b4e9e0b895f9a516ae7223
chikombe <chikombe.ecy@gmail.com>
2023-08-11 07:55:58 -0700

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """"Matches the provided Python bytecode"""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for itr in range(4, 6):
+            c = add(c, itr)
+            return(c)
+        else:
+            return(sub(a, b))
