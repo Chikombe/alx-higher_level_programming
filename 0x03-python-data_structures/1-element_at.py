fd047b00577bb455ab5e5d65d2ad6218ef9ee478
chikombe <chikombe.ecy@gmail.com>
2023-08-14 12:16:49 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+def element_at(my_list, idx):
+    """Retrieve an element from a list like in C."""
+    if index < 0 or index > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
