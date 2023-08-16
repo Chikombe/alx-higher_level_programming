7982fb85ef98cec493a18a69d7243bf003c2ec99
chikombe <chikombe.ecy@gmail.com>
2023-08-16 10:05:18 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    
+    for itr in range (len(matrix)):
+        new_matrix[itr] = list(map(lambda x: x**2, matrix[itr]))
+
+        return (new_matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+This repository contains exercises and examples related to Python's more advanced data structures: Sets and Dictionaries.
+These data structures offer powerful ways to handle and manipulate data efficiently. 
