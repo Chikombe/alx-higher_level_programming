d7eb712662badf55c3f8779b5524dec50b367cbc
chikombe <chikombe.ecy@gmail.com>
2023-08-28 13:06:58 -0700

    2-safe_print_list_integers.py 2-main.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,5 +17,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ret_val += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (ret_val)
+    print("")
+    return (ret_val)
