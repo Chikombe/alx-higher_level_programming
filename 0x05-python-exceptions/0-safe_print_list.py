b10d5930fce69e6fc6e759ff76f5990c1de1cff0
chikombe <chikombe.ecy@gmail.com>
2023-08-28 12:28:34 -0700

    amended 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,5 +17,5 @@ def safe_print_list(my_list=[], x=0):
             ret_val += 1
         except IndexError:
             break
-        print("")
-        return (ret_val)
+    print("")
+    return (ret_val)
