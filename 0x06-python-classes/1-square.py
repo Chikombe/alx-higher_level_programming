ffd5c4fd8665af682f335fad950cef7cd56cc437
chikombe <chikombe.ecy@gmail.com>
2023-08-29 22:46:26 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A class Square that defines a square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """"Initiaize a new Square.
+
+        Args:
+        size(int): The size of the new square."""
+
+        self_size = size
