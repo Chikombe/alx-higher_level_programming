9a64f3d8006ee6824abed699194e602421fd0164
Chikombe <chikombe.ecy@gmail.com>
2023-09-04 14:28:41 -0700

    Amended 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -64,16 +64,16 @@ class Rectangle:
         """Returns the rectangle with the greater area.
 
         Arguments:
-            rect_1: The first rectangle
-            rect_2: The second rectangle
+            rect_1 (Rectangle): The first rectangle
+            rect_2 (Rectangle): The second rectangle
         Raises:
-            TypeError: if either rect_1 or rect_2 is not a rectangle."""
+            TypeError: if either rect_1 or rect_2 is not a Rectangle."""
 
-        if not isinstance(rect_1)
+        if not isinstance(rect_1, Rectangle)
         raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2)
+        if not isinstance(rect_2, Rectangle)
         raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.arear():
+        if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
 
