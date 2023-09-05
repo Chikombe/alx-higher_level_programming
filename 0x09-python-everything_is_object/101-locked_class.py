2dfdcd729189d1d36fd24418195bf5b32bed1d6b
Chikombe <chikombe.ecy@gmail.com>
2023-09-05 12:48:10 -0700

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a locked class"""
+
+
+class LockedClass:
+    """Prevent the user from instantiating new LockdClass attributes
+    for anything but attributes called 'first_name."""
+
+    __slots__ = ["first_name"]
