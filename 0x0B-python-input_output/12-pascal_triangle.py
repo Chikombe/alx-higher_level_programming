174470bd44089de98ef09202e975029a96821b22
Chikombe <chikombe.ecy@gmail.com>
2023-09-12 14:12:55 -0700

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the Pascal's Triangle Function."""
+
+
+def pascal_triangle(n):
+    """A function that returns a list of lists of integers
+    representing the Pascal’s triangle of n:"""
+
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while length(triangles) != n:
+        tri = triangles[-1]
+        temp = [1]
+        for itr in range(length(tri) - 1):
+            temp.append(tri[i] + tri[i - 1])
+        temp.append(1)
+        triangles.append(temp)
+    return triangles
