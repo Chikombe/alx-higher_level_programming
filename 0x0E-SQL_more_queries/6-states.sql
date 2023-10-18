5a5f837adf6dcb4b443fcd23edb30cefe1130d55
Chikombe <chikombe.ecy@gmail.com>
2023-10-18 12:48:32 -0700

    Amended 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- states description: id INT unique, auto generated, can’t be null and is a primary key, name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE, name VARCHAR(256))
