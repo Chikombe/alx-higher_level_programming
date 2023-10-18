c6c70335b905cafb3629fdcde15105491ada1688
Chikombe <chikombe.ecy@gmail.com>
2023-10-18 14:19:24 -0700

    fixed errors

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ CREATE A DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Use a database
 USE hbtn_0d_usa;
 -- Creates a table
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS cities
 (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 	state_id INT NOT NULL,
