553d4f059c0009a765b81852b747a12ce4176c6a
Chikombe <chikombe.ecy@gmail.com>
2023-12-02 10:51:12 -0800

    Created various files

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" A script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa:"""
+""" A script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
@@ -9,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
